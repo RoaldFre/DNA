2bec74594b78c9b4ec7466fc7f47114ad1288a97
Roald Frederickx <roald.frederickx@gmail.com>
2011-12-16 22:46:44 +0100

    prettified: connecting cilinders instead of lines

--- render.c
+++ render.c
@@ -6,8 +6,11 @@
 #include "render.h"
 #include "system.h"
 
-#define SCREEN_W 800
-#define SCREEN_H 800
+#define SCREEN_W 1000
+#define SCREEN_H 1000
+
+#define CILINDER_FACES 8
+#define CILINDER_RADIUS (config.radius / 4)
 
 typedef struct {
 	GLfloat x, y, z;
@@ -21,6 +24,7 @@ const GLfloat light_ambi[] = {0.8, 0.8, 0.8, 0.0};
 const GLfloat red[]   = {1.0, 0.0, 0.0, 0.0};
 const GLfloat green[] = {0.0, 1.0, 0.0, 0.0};
 const GLfloat blue[]  = {0.0, 0.0, 1.0, 0.0};
+const GLfloat gray[]  = {0.1, 0.1, 0.1, 0.0};
 
 static int numVertices;
 static int numIndices;
@@ -33,6 +37,7 @@ static GLfloat view_angle;
 static void createSphere(int slices, int *numVert, Vertex3 **vertices, int *numInd,
 		GLushort **indices);
 static void drawLine(Vec3 *p1, Vec3 *p2);
+static void drawCilinder(Vec3 *p1, Vec3 *p2, int faces, double radius);
 static void renderParticles(int num, Particle *ps);
 static void gluPerspective(GLfloat fovy, GLfloat aspect, GLfloat zNear, 
 		GLfloat zFar);
@@ -93,11 +98,11 @@ bool stepGraphics()
 						config.timeStep / TIME_FACTOR);
 				break;
 			case SDLK_SPACE:
-				config.thermostatTemp *= 1.2;
+				config.thermostatTemp *= 1.1;
 				printf("Temperature: %f\n", config.thermostatTemp);
 				break;
 			case SDLK_BACKSPACE:
-				config.thermostatTemp /= 1.2;
+				config.thermostatTemp /= 1.1;
 				printf("Temperature: %f\n", config.thermostatTemp);
 				break;
 			case SDLK_RETURN:
@@ -220,17 +225,21 @@ int render(void)
 	renderParticles(config.numMonomers, world.Ps);
 
 	/* Connections */
-	glColor3f(0.1, 0.1, 0.1);
-	glBegin(GL_LINES);
-		drawLine(&world.Ps[0].pos, &world.Ss[0].pos);
-		drawLine(&world.Ss[0].pos, &world.As[0].pos);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, gray);
+	drawCilinder(&world.Ps[0].pos, &world.Ss[0].pos,
+			CILINDER_FACES, CILINDER_RADIUS);
+	drawCilinder(&world.Ss[0].pos, &world.As[0].pos,
+			CILINDER_FACES, CILINDER_RADIUS);
 		for(int i = 1; i < config.numMonomers; i++) {
-			drawLine(&world.Ps[i].pos, &world.Ss[i].pos);
-			drawLine(&world.Ss[i].pos, &world.As[i].pos);
-			drawLine(&world.Ss[i].pos, &world.Ps[i-1].pos);
+			drawCilinder(&world.Ps[i].pos, &world.Ss[i].pos,
+					CILINDER_FACES, CILINDER_RADIUS);
+			drawCilinder(&world.Ss[i].pos, &world.As[i].pos,
+					CILINDER_FACES, CILINDER_RADIUS);
+			drawCilinder(&world.Ss[i].pos, &world.Ps[i-1].pos,
+					CILINDER_FACES, CILINDER_RADIUS);
 		}
-	glEnd();
 
+#if 0
 	/* Forces */
 	glColor3f(0.8, 0.0, 0.0);
 	glBegin(GL_LINES);
@@ -240,22 +249,63 @@ int render(void)
 			drawLine(&world.all[i].pos, &tmp);
 		}
 	glEnd();
-
-
+#endif
 
 	SDL_GL_SwapBuffers();
 
 	return 0;
 }
 
-static void drawLine(Vec3 *p1, Vec3 *p2)
+static void drawPoint(Vec3 *p)
 {
 	double ws = config.worldSize;
-
-	glVertex3f(p1->x - ws/2, p1->y - ws/2, p1->z - ws/2);
-	glVertex3f(p2->x - ws/2, p2->y - ws/2, p2->z - ws/2);
+	glVertex3f(p->x - ws/2, p->y - ws/2, p->z - ws/2);
 }
 
+static void drawLine(Vec3 *p1, Vec3 *p2)
+{
+	drawPoint(p1);
+	drawPoint(p2);
+}
+
+static void drawCilinder(Vec3 *p1, Vec3 *p2, int faces, double radius)
+{
+	Vec3 d, dnorm;
+	sub(p2, p1, &d);
+	normalize(&d, &dnorm);
+	Vec3 e1 = {1, 0, 0};
+	Vec3 e2 = {0, 1, 0};
+	/* Make orthogonal basis for cilinders */
+	Vec3 b1, b2;
+	if (dot(&e1, &dnorm) < 0.8)
+		b1 = cross(&e1, &dnorm);
+	else
+		b1 = cross(&e2, &dnorm);
+	normalize(&b1, &b1);
+	b2 = cross(&b1, &dnorm);
+
+	assert(fabs(length(&b1) - 1) < 1e-13);
+	assert(fabs(length(&b2) - 1) < 1e-13);
+
+	scale(&b1, radius, &b1);
+	scale(&b2, radius, &b2);
+
+	glBegin(GL_QUAD_STRIP);
+		Vec3 l, r, u, v, dir;
+		add(p1, &b1, &l);
+		add(p2, &b1, &r);
+		drawLine(&l, &r);
+		for (int i = 1; i <= faces; i++) {
+			double theta = 2*M_PI * i / faces;
+			scale(&b1, cos(theta), &u);
+			scale(&b2, sin(theta), &v);
+			add(&u, &v, &dir);
+			add(p1, &dir, &l);
+			add(p2, &dir, &r);
+			drawLine(&l, &r);
+		}
+	glEnd();
+}
 
 static void renderParticles(int num, Particle *ps)
 {
