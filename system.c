110ca2f84d1c4f9cb8a8074a7670e91c8c222ed6
Roald Frederickx <roald.frederickx@gmail.com>
2012-03-21 00:59:54 +0100

    style and logic changes in basePairForce

--- system.c
+++ system.c
@@ -376,8 +376,8 @@ static void strandForces(Strand *s) {
 	}
 }
 
-static void basePairForce(Particle *p1, Particle *p2) {
-		
+static void basePairForce(Particle *p1, Particle *p2)
+{
 		double rij = distance(&p1->pos, &p2->pos);
 		double bp_coupling;
 		double bp_force_distance;
@@ -393,46 +393,34 @@ static void basePairForce(Particle *p1, Particle *p2) {
 		 * For the force we differentiate with respect to r,so we get
 		 * bp_coupling * 60*[ r^0^10 / r^11 - r^0^12/r^13 ] */	
 		
-		if ( (p1->type==BASE_A && p2->type==BASE_T) 
-							|| (p1->type==BASE_T && p2->type==BASE_A)) {
+		if ((p1->type==BASE_A && p2->type==BASE_T) 
+				|| (p1->type==BASE_T && p2->type==BASE_A)) {
 			bp_coupling = COUPLING_BP_AT;
 			bp_force_distance = DISTANCE_r0_AT;
 			
-			double rfrac = bp_force_distance / rij;
-			double rfrac2 = rfrac * rfrac;
-			double rfrac4 = rfrac2 * rfrac2;
-			double rfrac8 = rfrac4 * rfrac4;
-			double rfrac10 = rfrac8 * rfrac2;
-			double rfrac12 = rfrac10 * rfrac2;
-						
-			force = bp_coupling*60*(rfrac12 / rij -  rfrac10 / rij );
-			
-		} else if ( (p1->type==BASE_G && p2->type==BASE_C) 
-							|| (p1->type==BASE_C && p2->type==BASE_G)) {
+		} else if ((p1->type==BASE_G && p2->type==BASE_C) 
+				|| (p1->type==BASE_C && p2->type==BASE_G)) {
 			bp_coupling = COUPLING_BP_GC;
 			bp_force_distance = DISTANCE_r0_GC;
-		
-			double rfrac = bp_force_distance / rij;
-			double rfrac2 = rfrac * rfrac;
-			double rfrac4 = rfrac2 * rfrac2;
-			double rfrac8 = rfrac4 * rfrac4;
-			double rfrac10 = rfrac8 * rfrac2;
-			double rfrac12 = rfrac10 * rfrac2;
-						
-			force = bp_coupling*60*( rfrac12 / rij - rfrac10 / rij );
-		
-							
 		} else {
-			force = 0;
+			return; /* no force */
 		}
+
+		double rfrac = bp_force_distance / rij;
+		double rfrac2 = rfrac * rfrac;
+		double rfrac4 = rfrac2 * rfrac2;
+		double rfrac8 = rfrac4 * rfrac4;
+		double rfrac10 = rfrac8 * rfrac2;
+		double rfrac12 = rfrac10 * rfrac2;
+					
+		force = bp_coupling*60*( rfrac12 / rij - rfrac10 / rij );
 		
 		/* calculate the direction of the force between the basepairs */
 		Vec3 direction;
-		Vec3 direction_nonnorm;
 		
 		/* subtract and normalize */
-		sub(&p1->pos, &p2->pos, &direction_nonnorm);
-		normalize(&direction_nonnorm, &direction);
+		sub(&p1->pos, &p2->pos, &direction);
+		normalize(&direction, &direction);
 		
 		/* scale the direction with the calculated force */
 		scale(&direction, force, &force_vec);
