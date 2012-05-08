1d4ae536bf4615250afeb477d793c15a8a461a5c
Roald Frederickx <roald.frederickx@gmail.com>
2012-05-08 15:34:49 +0200

    scripts for simulation runs and plotting

---
+++ scripts/plotBasePairing.m
@@ -0,0 +1,12 @@
+function plotBasePairing(filename)
+data = load(filename);
+dataSize = size(data);
+nSamples = dataSize(1);
+nMonomers = dataSize(2) - 3; 
+	%last two columns are just total number of (correctly) bonded 
+	%pairs, the one before that holds the time
+
+time = data(:, nMonomers + 1);
+config = data(:, 1:nMonomers); %monomer configuration
+
+imagesc(time, 1:nMonomers, data(1:nSamples, 1:nMonomers)');
