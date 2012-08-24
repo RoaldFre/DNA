7d353488916932c8b9e3d43578c572166e4334ed
Roald Frederickx <roald.frederickx@gmail.com>
2012-08-24 17:48:53 +0200

    Changed scripts to reflect -H changes

--- scripts/hairpinFormationSingleRunNoExclusion.sh
+++ scripts/hairpinFormationSingleRunNoExclusion.sh
@@ -22,6 +22,8 @@ wait=0
 fps=10
 #render="-r -R 3 -f $fps"
 
+requiredBound=`echo "$N - $allowedUnBounded" | bc`
+
 
 outputBaseDir=`mktemp -d`
 outputFile="$outputBaseDir/outputFrom_${suffix}"
@@ -31,7 +33,7 @@ destinationFile="$destinationDir/formation_${suffix}"
 mkdir -p $destinationDir
 
 seq=`perl -e "print 'C'x$N,'G'x$N"`
-$main -t $timestep -E $zippingTemp -T $zippingTemp -I $interval -P $time -W $wait -s $seq -D $outputFile -H $allowUnbounded -M $allowBounded -O $zippingTemp -Q $unzippingTemp -U $zippedRelaxation -X f
+$main -t $timestep -E $zippingTemp -T $zippingTemp -I $interval -P $time -W $wait -s $seq -D $outputFile -H $requiredBound -M $allowBounded -O $zippingTemp -Q $unzippingTemp -U $zippedRelaxation -X f
 mv ${outputFile} ${destinationFile}
 
 rm -rf $outputBaseDir
