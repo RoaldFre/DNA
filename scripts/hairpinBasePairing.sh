62624deaf53a41f24afa0a72c5ce1600daf0063a
Roald Frederickx <roald.frederickx@gmail.com>
2012-09-09 15:25:16 +0200

    script to dump raw base pairing

---
+++ scripts/hairpinBasePairing.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# just dumps the base pairing info
+# Useful to compute the melting temperature yourself
+
+set -e
+
+destinationDirRoot=$1
+seq=$2
+temperature=$3
+time=$4
+suffix=$5 # job label when running on cluster
+
+saltConcentration=100
+
+main=hairpin # Should be in current working dir, or in $PATH
+timestep=15
+interval=20
+
+fps=10
+#render="-r -R 3 -f $fps"
+
+
+outputBaseDir=`mktemp -d`
+outputFile="$outputBaseDir/outputFrom_${suffix}"
+destinationDir="$destinationDirRoot/dt${timestep}_time${time}_salt${saltConcentration}/T${temperature}"
+destinationFile="$destinationDir/meltingTemp_${suffix}"
+
+mkdir -p $destinationDir
+
+endToEnd="-e" #while we're at it, sample this as well
+
+$main -t $timestep -I $interval -s $seq -D $outputFile -T $temperature -N $saltConcentration -P $time -p $endToEnd
+mv ${outputFile}_basePairing ${destinationFile}_basePairing
+mv ${outputFile}_endToEnd ${destinationFile}_endToEnd
+
+rm -rf $outputBaseDir
+
