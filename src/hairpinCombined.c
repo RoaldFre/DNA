3373ef85c9a43c3b0e60c52e80598408470ac87e
Roald Frederickx <roald.frederickx@gmail.com>
2012-09-15 15:33:30 +0200

    hairpinCombined: "fully force" LTO myself

---
+++ src/hairpinCombined.c
@@ -0,0 +1,22 @@
+/* This 'simulates' LTO by putting everything in one single source file. 
+ * The result is sometimes faster, even if the usual compilation is done 
+ * with LTO! */
+
+#define NO_RENDER
+
+#include "system.c"
+#include "math.c"
+#include "task.c"
+#include "measure.c"
+#include "samplers.c"
+#include "physics.c"
+#include "world.c"
+#include "spgrid.c"
+#include "tinymt/tinymt64.c"
+#include "render.c"
+#include "octave.c"
+//#include "font.c"
+//#include "mathlib/vector.c"
+//#include "mathlib/quaternion.c"
+//#include "mathlib/matrix.c"
+#include "hairpin.c"
