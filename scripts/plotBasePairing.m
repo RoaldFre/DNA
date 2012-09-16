c0e54e3ddfd14a7711c967915fde54d76dfb6d48
Roald Frederickx <roald.frederickx@gmail.com>
2012-09-17 00:39:22 +0200

    plotBasePairing.m: got pimped

--- scripts/plotBasePairing.m
+++ scripts/plotBasePairing.m
@@ -1,4 +1,18 @@
-function plotBasePairing(filename)
+function plotBasePairing(filename, plotState, plotNumBasePairs, numBPsDecimateFactor)
+
+addpath("octave-forge");
+
+if nargin == 1
+	plotState = true;
+	plotNumBasePairs = false;
+	numBPsDecimateFactor = 1;
+elseif nargin == 2
+	plotNumBasePairs = false;
+	numBPsDecimateFactor = 1;
+elseif nargin == 3
+	numBPsDecimateFactor = 1;
+end
+
 data = load(filename);
 dataSize = size(data);
 nSamples = dataSize(1);
@@ -6,10 +20,18 @@ nMonomers = dataSize(2) - 3;
 	%last two columns are just total number of (correctly) bonded 
 	%pairs, the one before that holds the time
 
-time = data(:, nMonomers + 1);
-config = data(:, 1:nMonomers); %monomer configuration
+config         = data(:, 1:nMonomers); %monomer configuration
+time           = data(:, nMonomers + 1);
+allBound       = data(:, nMonomers + 2);
+correctlyBound = data(:, nMonomers + 3);
 
+if plotNumBasePairs
+	q = numBPsDecimateFactor;
+	plot(decimate(time, q), decimate(allBound, q), 'k', ...
+	     decimate(time, q), decimate(correctlyBound,q), 'b');
+end
 
-
-imagesc(time, 1:nMonomers, data(1:nSamples, 1:nMonomers)');
-colormap(bone(2));
+if plotState
+	imagesc(time, 1:nMonomers, data(1:nSamples, 1:nMonomers)');
+	colormap(bone(2));
+end
