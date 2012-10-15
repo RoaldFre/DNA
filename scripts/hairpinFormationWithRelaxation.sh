10d83a746b3452b8728b3c8b27aeb03e17ba6dbc
Roald Frederickx <roald.frederickx@gmail.com>
2012-10-15 19:36:40 +0200

    don't allow bounded, allow unbounded *fraction*

--- scripts/hairpinFormationWithRelaxation.sh
+++ scripts/hairpinFormationWithRelaxation.sh
@@ -12,36 +12,33 @@ N=$3
 relaxTemp=$4
 zippingTemp=$5
 unzippingTemp=$6
-allowUnbounded=$7
+allowedUnboundedFraction=$7
 zippedRelaxation=$8
-time=$9 # this is a cut off: if there is no hairpin by now: give up! XXX SET LARGE ENOUGH!
-suffix=${10} # job label when running on cluster
+suffix=$9 # job label when running on cluster
 
-allowBounded=0
 
 main=hairpin # Should be in current working dir, or in $PATH
 timestep=15
-interval=50
+interval=100
+
+echo "Computing required bounded base pairs"
+# Yes, ugly.
+requiredBounded=`python -c "print(int(round($N * (1 - $allowedUnboundedFraction))))"`
+allowedBounded=0
+echo "requiredBounded: $requiredBounded"
+
 
 echo "Getting wait time"
-
 wait=`$scriptsdir/hairpinRelaxationTime.py $N`
-
 echo "Wait time: $wait"
 
 
-requiredBound=`echo "$N - $allowUnbounded" | bc`
-
 outputBaseDir=`mktemp -d`
 outputFile="$outputBaseDir/outputFrom_${suffix}"
-destinationDir="$destinationDirRoot/$fullSequence/relaxT${relaxTemp}_zipT${zippingTemp}_unzipT${unzippingTemp}_allowUnb${allowUnbounded}_zippedRel${zippedRelaxation}/dt${timestep}_time${time}/N${N}"
+destinationDir="$destinationDirRoot/$fullSequence/relaxT${relaxTemp}_zipT${zippingTemp}_unzipT${unzippingTemp}_allowUnb${allowedUnboundedFraction}_zippedRel${zippedRelaxation}/dt${timestep}/N${N}"
 destinationFile="$destinationDir/formation_${suffix}"
 
-
 echo "Temporary directory is $outputBaseDir"
-echo "Making destination directory: $destinationDir"
-
-mkdir -p $destinationDir
 
 echo "Making sequence"
 
@@ -50,14 +47,18 @@ seq=`$scriptsdir/hairpinFromStem.py $fullSequence $N`
 echo "Sequence is: $seq"
 echo "Starting main program!"
 
-$main -t $timestep -T $relaxTemp -I $interval -P $time -W $wait -s $seq -D $outputFile -H $requiredBound -M $allowBounded -O $zippingTemp -Q $unzippingTemp -U $zippedRelaxation -X f -k -e
+$main -t $timestep -T $relaxTemp -I $interval -W $wait -s $seq -D $outputFile -H $requiredBounded -M $allowedBounded -O $zippingTemp -Q $unzippingTemp -U $zippedRelaxation -X f -k -e
+
+
+echo "Making destination directory: $destinationDir"
+mkdir -p $destinationDir
 
 echo "Moving output files!"
-
 mv ${outputFile} ${destinationFile}
 mv "${outputFile}_temperature" "${destinationFile}_temperature" #for verification / extra info
 mv "${outputFile}_endToEnd" "${destinationFile}_endToEnd" #for verification / extra info
 
+echo "Deleting temporary directory"
 rm -rf $outputBaseDir
 
 echo "All done! *High five*!"
