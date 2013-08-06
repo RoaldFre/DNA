810edeb2bbbc5c0c6a0e1059d73cdd5ece964d2f
Roald Frederickx <roald.frederickx@gmail.com>
2013-08-06 16:28:48 +0200

    extractResults: cleaned some local/global vars

--- scripts/extractResults.sh
+++ scripts/extractResults.sh
@@ -2,16 +2,26 @@
 
 meanStartN=20
 Ns=5.6.7.8.10.12.14.17.20.24.28.34.40.48
+loglog=1
+SA=0
+
 
 resultsdir=$HOME/DNA/scripts/results
+
+makeSuffix() {
+	singleExponent=$1
+	sqDev=$2
+	echo "${scalingType}_singexp${singleExponent}_2times0_sqdev${sqDev}_loglog${loglog}_SA${SA}"
+}
 
 extractField() {
 	field="$1"
 	field=`printf '%q' "$field"` # escape special characters
 	#field=`printf '%q' "$field"` # escape special characters
 	singleExponent=$2
+	sqDev=$3
 
-	file="$resultsdir/${name}T${T}_result${Ns}meanStartN${meanStartN}_${scalingType}_singexp${singleExponent}_2times0_sqdev${sqdev}_loglog${loglog}_SA${SA}"
+	file="$resultsdir/${name}T${T}_result${Ns}meanStartN${meanStartN}_`makeSuffix $singleExponent $sqDev`"
 
 	if ! sed -n "s#^${field}:##p" $file
 	then
@@ -24,46 +34,40 @@ extractField() {
 
 extractLine() {
 	# Extract a line from the cached resultfiles
-	# Currently hardcoded to make the first column of T=10 a 2-row multirow, 
-	# and the first column of all other T's (eg T=30) a blank space (to work 
-	# with the multirow of T=10)
+	# Currently hardcoded to make the first column of T=10 a 2-row 
+	# multirow, and the first column of all other T's (eg T=30) a blank 
+	# space (to work with the multirow of T=10)
 
 	name=$1
 	T=$2
-	scalingType=$3 #withTime or withSize
-	sqdev=$4 # 1 or 0
-	Ns=$5
-	meanStartN=$6
-
-	loglog=1
-	SA=0
+	sqDev=$3 # 1 or 0
 
 	if [ "$scalingType" == withTime ]
 	then
-		observable=`extractField 'observable' 0`
+		observable=`extractField 'observable' 0 $sqDev`
 
-		betaSing=`extractField '\beta' 0`
-		#deltaSing=`extractField 'theoreticalDelta' 0`
-		qualSing=`extractField 'qual' 0`
+		betaSing=`extractField '\beta' 0 $sqDev`
+		#deltaSing=`extractField 'theoreticalDelta' 0 $sqDev`
+		qualSing=`extractField 'qual' 0 $sqDev`
 
-		betaFull=`extractField '\beta' 1`
-		#alphaFull=`extractField '\alpha' 1`
-		alphaInvFull=`extractField '1/\alpha' 1`
-		qualFull=`extractField 'qual' 1`
+		betaFull=`extractField '\beta' 1 $sqDev`
+		#alphaFull=`extractField '\alpha' 1 $sqDev`
+		alphaInvFull=`extractField '1/\alpha' 1 $sqDev`
+		qualFull=`extractField 'qual' 1 $sqDev`
 
 		#pmstring: string with original \pm plusminus
 		pmString="$betaSing & $qualSing & $betaFull & $alphaInvFull & $qualFull"
 	elif [ "$scalingType" == withSize ]
 	then
-		observable=`extractField 'observable' 0`
+		observable=`extractField 'observable' 0 $sqDev`
 
-		betaSing=`extractField '\beta' 0`
-		deltaSing=`extractField 'theoreticalDelta' 0`
-		qualSing=`extractField 'qual' 0`
+		betaSing=`extractField '\beta' 0 $sqDev`
+		deltaSing=`extractField 'theoreticalDelta' 0 $sqDev`
+		qualSing=`extractField 'qual' 0 $sqDev`
 
-		betaFull=`extractField '\beta' 1`
-		deltaFull=`extractField '\delta' 1`
-		qualFull=`extractField 'qual' 1`
+		betaFull=`extractField '\beta' 1 $sqDev`
+		deltaFull=`extractField '\delta' 1 $sqDev`
+		qualFull=`extractField 'qual' 1 $sqDev`
 
 		#pmstring: string with original \pm plusminus
 		pmString="$betaSing & \$$deltaSing\$ & $qualSing & $betaFull & $deltaFull & $qualFull"
@@ -87,7 +91,7 @@ extractLine() {
 
 echo 
 echo 
-scalingType=withTime
+scalingType=withTime # This is a global, also used in functions above!
 echo    '\begin{tabular}{cc | r@{$\pm$}l  r@{$\pm$}l | r@{$\pm$}l  r@{$\pm$}l  r@{$\pm$}l}'
 
 echo -n '\multirow{2}{*}{observable} &'
@@ -102,14 +106,14 @@ echo -n '\multicolumn{2}{c}{$\beta$} &'
 echo -n '\multicolumn{2}{c}{$1/\alpha$} &'
 echo    '\multicolumn{2}{c}{$Q$} \\\hline'
 
-echo "`extractLine state 10 $scalingType 0 $Ns $meanStartN`\\\\"
-echo "`extractLine state 30 $scalingType 0 $Ns $meanStartN`\\\\\\hline"
-echo "`extractLine energy 10 $scalingType 1 $Ns $meanStartN`\\\\"
-echo "`extractLine energy 30 $scalingType 1 $Ns $meanStartN`\\\\\\hline"
-echo "`extractLine gyrad 10 $scalingType 1 $Ns $meanStartN`\\\\"
-echo "`extractLine gyrad 30 $scalingType 1 $Ns $meanStartN`\\\\\\hline"
-echo "`extractLine endToEnd 10 $scalingType 1 $Ns $meanStartN`\\\\"
-echo "`extractLine endToEnd 30 $scalingType 1 $Ns $meanStartN`\\\\"
+echo "`extractLine state 10 0`\\\\"
+echo "`extractLine state 30 0`\\\\\\hline"
+echo "`extractLine energy 10 1`\\\\"
+echo "`extractLine energy 30 1`\\\\\\hline"
+echo "`extractLine gyrad 10 1`\\\\"
+echo "`extractLine gyrad 30 1`\\\\\\hline"
+echo "`extractLine endToEnd 10 1`\\\\"
+echo "`extractLine endToEnd 30 1`\\\\"
 
 echo    '\end{tabular}'
 
@@ -120,7 +124,7 @@ echo
 echo 
 
 
-scalingType=withSize
+scalingType=withSize # This is a global, also used in functions above!
 echo    '\begin{tabular}{cc | r@{$\pm$}l c r@{$\pm$}l | r@{$\pm$}l  r@{$\pm$}l  r@{$\pm$}l}'
 
 echo -n '\multirow{2}{*}{observable} &'
@@ -136,15 +140,15 @@ echo -n '\multicolumn{2}{c}{$\beta$} &'
 echo -n '\multicolumn{2}{c}{$\delta$} &'
 echo    '\multicolumn{2}{c}{$Q$} \\\hline\hline'
 
-echo "`extractLine state 10 $scalingType 0 $Ns $meanStartN`\\\\"
-echo "`extractLine state 30 $scalingType 0 $Ns $meanStartN`\\\\\\hline"
-echo "`extractLine energy 10 $scalingType 1 $Ns $meanStartN`\\\\"
-echo "`extractLine energy 30 $scalingType 1 $Ns $meanStartN`\\\\\\hline"
-echo "`extractLine gyrad 10 $scalingType 0 $Ns $meanStartN`\\\\"
-echo "`extractLine gyrad 30 $scalingType 0 $Ns $meanStartN`\\\\\\hline"
-echo "`extractLine gyrad 10 $scalingType 1 $Ns $meanStartN`\\\\"
-echo "`extractLine gyrad 30 $scalingType 1 $Ns $meanStartN`\\\\\\hline"
-echo "`extractLine endToEnd 10 $scalingType 1 $Ns $meanStartN`\\\\"
-echo "`extractLine endToEnd 30 $scalingType 1 $Ns $meanStartN`\\\\"
+echo "`extractLine state 10 0`\\\\"
+echo "`extractLine state 30 0`\\\\\\hline"
+echo "`extractLine energy 10 1`\\\\"
+echo "`extractLine energy 30 1`\\\\\\hline"
+echo "`extractLine gyrad 10 0`\\\\"
+echo "`extractLine gyrad 30 0`\\\\\\hline"
+echo "`extractLine gyrad 10 1`\\\\"
+echo "`extractLine gyrad 30 1`\\\\\\hline"
+echo "`extractLine endToEnd 10 1`\\\\"
+echo "`extractLine endToEnd 30 1`\\\\"
 
 echo    '\end{tabular}'
